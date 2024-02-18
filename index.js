d45cae696f22efd32e3f6904df25f010eb4c3dea
Qingqing Yang <qingqingyang@hotmail.co.uk>
2024-02-18 18:51:12 +0000

    add questions in array

--- index.js
+++ index.js
@@ -5,7 +5,52 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'userName',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is your project`s name?',
+        name: 'projectName',
+    },
+    {
+        type: 'input',
+        message: 'Please write a short description of your project:',
+        name: 'description',
+    },
+    {
+        type: 'list',
+        message: 'What kind of license should your project have?',
+        name: 'license',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to install dependencies?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What does the user need to know about contributing to the repo?',
+        name: 'contributing',
+    },
 ];
 
 // function to write README file
@@ -14,7 +59,11 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+        })
 }
 
 // function call to initialize program
